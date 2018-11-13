73b949dbabad09961c39e06afd1265e64677bdf8
Addison Wesley Boyer <addisonwesleyboyer@Addisons-MacBook-Air.local>
2018-11-13 01:47:53 -0700

    Added sound effects, bustIts functionality, and new questions with ranging difficulty

--- typerdraw.js
+++ typerdraw.js
@@ -3,9 +3,9 @@ Author: Addison Boyer
 Created: 11/02/2018
 Shell for tetris style game for TyperPython
 */
+var crumbling = new Audio("./crumbling.mp3")
 var correct = new Audio("./correct.mp3");
-var question = {"question": "1,2,..,5", "answer": "1\n2\n3\n4\n5\n"};
-console.log(question.answer);
+var difficulty = 2, score = 0, bustIts = 3; 
 var canvas = document.getElementById("gameback");
 ctx = canvas.getContext("2d");
 canvas.height = 500;
@@ -76,7 +76,7 @@ function update(e){
             
             // Collides with another block
             if(collide(blocks[i])){
-                if(blocks[blockIndex][1]<= 0){
+                if(blocks[blockIndex][1]<= 0){ // Blocks have reached the top, game is over
                     window.alert("Game over!");
                     location.reload();
                 }
@@ -86,7 +86,7 @@ function update(e){
             }
         }
         
-        blocks[blockIndex][1]+=.5;
+        blocks[blockIndex][1]+=.2;
     }
     else{
         blocks[blockIndex][6] = false;
@@ -108,15 +108,16 @@ function draw(){
         ctx.strokeRect(b[0],b[1],b[3],b[4]);
         ctx.fillRect(b[0],b[1],b[3],b[4]);
         ctx.fillStyle = "black";
-        ctx.font = "12px Comic Sans MS";
+        ctx.font = "10px Comic Sans MS";
         ctx.fillText(b[5].question, b[0]+2,b[1]+b[4]/2);
     });
 }
 
 function spawnBlock(){
     dropX = Math.round(Math.random()*(canvas.width-canvas.width/7));
+console.log(dropX);
     // x[0], y[1], color[2], width[3], height[4] question[5] isActive[6]
- blocks.push([dropX,-20,colors[Math.round(Math.random()*3)],canvas.width/10,canvas.height/10,createQuestion(),true]);
+ blocks.push([dropX,-20,colors[Math.round(Math.random()*3)],canvas.width/7,canvas.height/7,createQuestion(),true]);
     blockIndex+=1;
     return;
 }
@@ -129,6 +130,10 @@ function checkForAnswer(answer){
             blocks.splice(blocks.indexOf(b),1);
             blockIndex-=1;
             correct.play();
+            score+=difficulty; // Score porportional to difficulty
+            if(score % 5 == 0){
+                difficulty+=1;
+            }
             document.getElementById("yourcode").value = "";
             spawnBlock();
         }
@@ -139,21 +144,69 @@ function checkForAnswer(answer){
 function createQuestion(){
     var start = Math.round(Math.random()*10);
     var stop = Math.round((Math.random()*20)+11);
-    var questionString = start + " to " + stop; 
+    
+    if(difficulty == 1){
+        var questionString = start + " to " + stop + " (Sum)"; 
+        var answerString = "";
+        total = 0;
+        for (var i = start; i <= stop; i++){
+            total+=i;
+        }
+        answerString+=total
+        answerString+="\n";
+    }
+    else if(difficulty == 2){
+        var divisibleBy = Math.round(Math.random(1)*10);
+        var questionString = start + " to " + stop + " %" + divisibleBy + "=0"; 
     var answerString = "";
     
-    for(var i = start; i<stop; i++){
-        answerString = answerString + i + "\n";
+    for(var i = start; i<=stop; i++){
+        
+        if(i % divisibleBy == 0){
+            answerString = answerString + i + "\n";   
+        }
+        
     }
+        
+    }
+    else{
+        
+        var questionString = start + " to " + stop; 
+        var answerString = "";
+    
+        for(var i = start; i<=stop; i++){
+            answerString = answerString + i + "\n";
+    }
+        
+    }
+    
     
 
     var question = {"question": questionString, "answer": answerString};
-    console.log(questionString);
-    console.log(answerString);
+    
     return question;
-} 
+}
 
-setInterval(update, 20);
+function killBlock(e){
+    var rect = canvas.getBoundingClientRect();
+    
+    var scaleX = canvas.width / rect.width;
+    var scaleY = canvas.height / rect.height;
+    
+    var x = (e.clientX - rect.left)*scaleX;
+    var y = (e.clientY - rect.top)*scaleY;
+    
+    if(collide([x,y,"Blue",30,30,true]) && bustIts > 0){
+        blocks.splice(blocks.indexOf(blocks[blockIndex]));
+            crumbling.play()
+            bustIts-=1;
+            blockIndex-=1;
+            spawnBlock();
+    }
+}
+
+document.onmousedown = killBlock;
+setInterval(update, 10);
 
 
 
